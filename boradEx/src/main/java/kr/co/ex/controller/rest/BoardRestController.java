fcc12f20534665bece2634b28f5d5c592fce4a53
smith <wndmstla123@gmail.com>
2019-03-21 23:51:27 +0900

    버그 수정

--- boradEx/src/main/java/kr/co/ex/controller/rest/BoardRestController.java
+++ boradEx/src/main/java/kr/co/ex/controller/rest/BoardRestController.java
@@ -12,26 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.ex.domain.BoardVO;
 import kr.co.ex.domain.Criteria;
-import kr.co.ex.domain.ReplyVO;
 import kr.co.ex.service.BoardService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
+@Log4j
 @RestController
 @RequiredArgsConstructor
-@Log4j
 @RequestMapping("/board/api")
 public class BoardRestController {
 	
-	@NonNull
-	private BoardService boardServ;
+	@NonNull private BoardService boardServ;
 	
 	@GetMapping(value="", produces={MediaType.APPLICATION_JSON_UTF8_VALUE})
 	public ResponseEntity<List<BoardVO>> getPosts(Criteria cri){
 		List<BoardVO> posts = null;
 		try {
-			posts = boardServ.listBoardByWriter(SecurityContextHolder.getContext().getAuthentication().getName(), cri);
+			posts = boardServ.listByWriter(SecurityContextHolder.getContext().getAuthentication().getName(), cri);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
