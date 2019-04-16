ac0076f5953b3d5918fea821182da194a71c1081
SimEunJu <wndmstla123@gmail.com>
2019-04-16 14:46:47 +0900

    파일 삭제 cron 로직 수정

--- boradEx/src/main/java/kr/co/ex/task/FileDeleteTask.java
+++ boradEx/src/main/java/kr/co/ex/task/FileDeleteTask.java
@@ -2,75 +2,53 @@ package kr.co.ex.task;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.TreeSet;
 
-import org.apache.ibatis.session.ResultContext;
-import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import kr.co.ex.util.DateUtils;
-import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-//@Component
+@Component
 @Log4j
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class FileDeleteTask {
 
-		@NonNull
-		private SqlSession sess;
-		@NonNull
-		private String uploadPath;
-		
-		private TreeSet<String> fileTree = new TreeSet<>();
-	
-		// 1. db에서 하루 동안 저장된 파일 목록 불러오기
-		// 2. resultHandler를 사용해서 처리
-		// 3. db에 저장되지 않은 것만 골라내느냐/ 폴더 내의 모든 파일 이름의 목록 -> 비교 -> 삭제 loop 3번
-	
+		@NonNull private SqlSession sess;
+		@NonNull private String uploadPath;
+
 		@Scheduled(cron="0 0 4 * * *")
 		public void deleteFiles(){
 			// 한 폴더에 하루 동안 생성된 모든 파일을 저장한다고 가정 + 양 많음
 			
 			LocalDateTime yesterday = DateUtils.getADaysAgo(1);
+			LocalDateTime todayMid = DateUtils.getADaysAgo(0);
 			String yesterdayPath = yesterday.format(DateTimeFormatter.ISO_LOCAL_DATE).replaceAll("-", File.pathSeparator);
 			
-			HashMap<String, Object> map = new HashMap<>();
-			map.put("yesterday", yesterday);
-			map.put("uploadPath", Paths.get(uploadPath, yesterdayPath));
-			sess.select("select uuid from tbl_attach where regdate>=#{yesterday} and upload_path=#{uploadPath}", map, new CustomResultHandler());
-			
 			String filePath = uploadPath + yesterdayPath;
-			String files[] = Paths.get(filePath).toFile().list();
-			for(String file : files){
-				String fileName;
-				
-				if(file.startsWith("s_")) fileName = file.substring(2);
-				else fileName = file;
-				
-				fileName = fileName.substring(0, fileName.lastIndexOf("_"));
-				if(!fileTree.contains(fileName))
-					try {
-						Files.deleteIfExists(Paths.get(filePath, file));
-					} catch (IOException e) {
-						e.printStackTrace();
-					} 
-			}
-		}
-	
-		private class CustomResultHandler implements ResultHandler<String>{
-			@Override
-			public void handleResult(ResultContext<? extends String> resultContext) {
-				fileTree.add(resultContext.getResultObject());
+			try(DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(filePath))){
+				for(Path entry : stream){
+					String fileName = entry.getFileName().toString();
+					if(fileName.startsWith("s_")) continue;
+					
+					String uuid = fileName.substring(0, fileName.lastIndexOf("_"));
+					String fileType = sess.selectOne("select file_type from tbl_attach where uuid='"+uuid+"'");
+					if(fileType != null) Files.delete(entry);
+					if(fileType.contains("image")) Files.delete(Paths.get(filePath, "s_"+fileName));
+				}
+			} catch (IOException e) {
+				log.error("file 삭제 cron io exception");
+				log.error(e);
+				e.printStackTrace();
 			}
 		}
 }
